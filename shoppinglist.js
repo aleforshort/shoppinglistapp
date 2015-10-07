d55feaeef39bb2d5a12ce6130d6c56d86fee4faf
Alejandra Diaz <diaz.rivera.alejandra@gmail.com>
2015-10-06 20:22:28 -0400

    update

--- shoppinglist.js
+++ shoppinglist.js
@@ -3,7 +3,8 @@ $(document).ready(function (){
 $("button.add-item").on("click", function(){
 	var grocery = $("input#addto").val(); 
 	$("list-items").append( "<li>" + grocery + "</li>");
-	})
+};
+
 });
 
 // // $(".addButton").on("click", function(){ var grocery = $("input").val(); 
